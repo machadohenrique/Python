ff429413664a985dba784d24b732deb44f8a17f0
Gabriel Henrique Machado <machadogast@gmail.com>
2023-04-08 13:53:34 -0300

    Create LacoFor.py

---
+++ LacoFor.py
@@ -0,0 +1,19 @@
+#for n in range(start, stop, step)
+
+#for i in range(2,5):
+#    print(i)
+
+
+#for i in range(2, 8, 2):
+#    print(i)
+    
+#for decrescente
+
+#for i in range(10, 0, -1):
+#    print(i)
+
+
+#percorrendo uma lista com "FOR"
+#for i in [0, 1, 2, 3, 4]:
+#    print(i)
+
