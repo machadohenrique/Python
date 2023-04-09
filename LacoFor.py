cec64818cb984f82dad3af0056d2fb58c26c1de3
Gabriel Henrique Machado <machadogast@gmail.com>
2023-04-09 15:42:23 -0300

    Update LacoFor.py
    Entendendo os laços de repetição FOR e WHILE.

--- LacoFor.py
+++ LacoFor.py
@@ -1,19 +1,31 @@
-#for n in range(start, stop, step)
-
-#for i in range(2,5):
+#for i in range(5):
 #    print(i)
 
 
-#for i in range(2, 8, 2):
+#for i in range(2,6):
+#    print(i)
+
+#for n in range(start, stop, step)
+#for i in range(2, 10, 3):
 #    print(i)
     
-#for decrescente
 
+#for decrescente
 #for i in range(10, 0, -1):
-#    print(i)
+#   print(i)
 
 
 #percorrendo uma lista com "FOR"
 #for i in [0, 1, 2, 3, 4]:
 #    print(i)
 
+
+
+#LAÇO WHILE
+'''
+i = 1
+while (i <= 5):
+    print(i)
+    i += 1 #i = i + 1
+    
+'''
