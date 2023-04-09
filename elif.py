439d7e8f90e3058b27b8d3d3ac81dab6adc97d16
Gabriel Henrique Machado <machadogast@gmail.com>
2023-04-09 15:41:53 -0300

    Create elif.py
    Revisão de Estruturas condicionais IF e ELSE.
    Aplicando a estrutura condicional ELIF.

---
+++ elif.py
@@ -0,0 +1,22 @@
+#REVISÃO IF ELSE
+'''
+a = int(input("Informe um numero: "))
+if a >= 50:
+    print ("O número ", a, " é maior ou igual a 50")
+else:
+    print ("O número ", a, " é menor que 50")
+
+'''
+
+
+'''
+
+a = int(input("Informe um número: "))
+if a > 50:
+    print ("O número ", a, " é maior que 50")
+elif a == 50:
+    print ("O número informado é igual a 50")
+else:
+  print ("O número ", a, " é menor que 50")
+
+'''
